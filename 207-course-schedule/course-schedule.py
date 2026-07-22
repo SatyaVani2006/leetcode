9b448a5b185b26fe7fbd5623b96a2aa7be785ac3
SatyaVani2006 <satyavanipilla2@gmail.com>
2026-07-22 14:18:58 +0530

    Time: 5 ms (54.79%) | Memory: 21.1 MB (38.60%) - LeetSync

---
+++ 207-course-schedule/course-schedule.py
@@ -0,0 +1,43 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        from collections import defaultdict
+from typing import List
+
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        g = defaultdict(list)
+
+        # Build graph
+        for a, b in prerequisites:
+            g[a].append(b)
+
+        UNVISITED = 0
+        VISITING = 1
+        VISITED = 2
+
+        states = [UNVISITED] * numCourses
+
+        def dfs(node):
+            state = states[node]
+
+            if state == VISITED:
+                return True
+
+            if state == VISITING:
+                return False
+
+            states[node] = VISITING
+
+            for nei in g[node]:
+                if not dfs(nei):
+                    return False
+
+            states[node] = VISITED
+            return True
+
+        for i in range(numCourses):
+            if not dfs(i):
+                return False
+
+        return True
+        
