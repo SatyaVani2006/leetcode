59e3fb54569947cbfb1aa4799204a4309ec1d150
SatyaVani2006 <satyavanipilla2@gmail.com>
2026-07-24 11:33:31 +0530

    Time: 29 ms (15.53%) | Memory: 19.8 MB (8.25%) - LeetSync

---
+++ 383-ransom-note/ransom-note.py
@@ -0,0 +1,10 @@
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        count={}
+        for char in magazine:count[char]=count.get(char,0)+1
+        for char in ransomNote:
+            if count.get(char,0)<=0:
+                return False
+            count[char]-=1
+        return True        
+        
