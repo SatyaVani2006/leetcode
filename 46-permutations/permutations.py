42211ffb086ac44ced7e5b5c3e27c8dcba98bcfb
SatyaVani2006 <satyavanipilla2@gmail.com>
2026-08-11 14:37:27 +0530

    Time: 1 ms (44.83%) | Memory: 19.5 MB (34.05%) - LeetSync

--- 46-permutations/permutations.py
+++ 46-permutations/permutations.py
@@ -1,5 +1,19 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        prem=permutations(nums)
-        return list(prem)
+        # prem=permutations(nums)
+        # return list(prem)
+        res=[]
+        sol=[]
+        n=len(nums)
+        def backtrack():
+            if len(sol)==n:
+                res.append(sol[::])
+                return
+            for x in nums:
+                if x not in sol:
+                    sol.append(x)
+                    backtrack()
+                    sol.pop()
+        backtrack()
+        return res                
         
